559160673d6f1aace5fcc0aa49895e9a6ec53d4e
skezics <zackstephens99@gmail.com>
2022-06-07 12:14:15 -0500

    completes function enroll

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -19,17 +19,17 @@ contract SimpleBank {
     // Fill in the visibility keyword
     // Hint: We want to create a getter function and allow contracts to be able
     //       to see if a user is enrolled.
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     // Let's make sure everyone knows who owns the bank, yes, fill in the
     // appropriate visilibility keyword
-    address owner = msg.sender;
+    address public owner = msg.sender;
     
     /* Events - publicize actions to external listeners
      */
     
     // Add an argument for this event, an accountAddress
-    event LogEnrolled();
+    event LogEnrolled(address);
 
     // Add 2 arguments for this event, an accountAddress and an amount
     event LogDepositMade();
@@ -63,6 +63,9 @@ contract SimpleBank {
     // Emit the appropriate event
     function enroll() public returns (bool){
       // 1. enroll of the sender of this transaction
+      enrolled[msg.sender] = true;
+      emit LogEnrolled(msg.sender);
+      return enrolled[msg.sender];
     }
 
     /// @notice Deposit ether into bank
